8fbd23e4266951006e86619645a65aba89317e6c
Hofe <hocheul920701@gmail.com>
2022-02-14 16:58:20 +0900

    test commit

--- auto_2.sh
+++ auto_2.sh
@@ -6,8 +6,9 @@ git branch -M main
 git remote add origin https://github.com/JungHocheul/WebCrawlingTest.git
 git push -u origin main
 
+sleep 2
 value=$(<./../email.txt)
 echo "$value"
-
+sleep 2
 value2=$(<./../token.txt)
 echo "$value2"
